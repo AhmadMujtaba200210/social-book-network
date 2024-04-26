1df397dd6e292d3ae213d90ab6ecf0d7b0851b35
Ahmad Mujtaba <ahmad.mujtaba@newbuffers.com>
2024-04-26 11:56:35 +0500

    feat: User & Token class implementing

---
+++ backend/src/main/java/io/engicodes/backend/security/JwtService.java
@@ -0,0 +1,10 @@
+package io.engicodes.backend.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUsername(String jwt) {
+        return null;
+    }
+}
---
+++ backend/src/main/java/io/engicodes/backend/user/Token.java
@@ -0,0 +1,30 @@
+package io.engicodes.backend.user;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Token {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(
+            name = "userId",
+            nullable = false
+    )
+    private User user;
+}
